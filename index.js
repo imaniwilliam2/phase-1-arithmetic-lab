312d951d6eb0a31e10a78b0ecc7e1ad3d9c3208a
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-01-29 16:08:43 -0500

    updated lab

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const num1 = 31
 const num2 = 2
 const multiply = num1 * num2
 
-const random = Math.floor(Math.random() * 1000) + 1
+const random = Math.floor(Math.random() * 10) + 1
 
-const num3 = 16
-const num4 = 4
-const mod = num3 / num4 
+const num3 = 4
+const num4 = 5
+const mod = num3 % num4 
 
 const max = Math.max(14, 16, 18, 20)
